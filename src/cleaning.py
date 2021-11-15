aa2cd871c8ff609df072cc90a32c27977e80805c
Tomás <tomaslouromendes@gmail.com>
2021-11-15 23:51:53 +0000

    small fix in columns

--- src/cleaning.py
+++ src/cleaning.py
@@ -5,12 +5,18 @@ from load_data import df_reviews as reviews, final_movies as movies
 
 
 #movies
-movies.drop(columns=["title", "authors", "year", "avg_vote", "votes", "metascore", "usa_gross_income", "reviews_from_users", "reviews_from_critics", "audience_count", "critics_consensus", "date_published", "tomatometer_status", "audience_status", "tomatometer_count", "audience_rating", "tomatometer_rating", "tomatometer_top_critics_count", "tomatometer_fresh_critics_count", "tomatometer_rotten_critics_count", "votes_10", "votes_9", "votes_8", "votes_7", "votes_6", "votes_5", "votes_4", "votes_3", "votes_2", "votes_1", "allgenders_0age_votes", "allgenders_0age_avg_vote", "allgenders_18age_avg_vote", "allgenders_18age_votes", "allgenders_30age_avg_vote", "allgenders_30age_votes", "allgenders_45age_avg_vote", "allgenders_45age_votes", "males_0age_avg_vote", "males_0age_votes", "males_18age_avg_vote", "males_18age_votes", "males_30age_avg_vote", "males_30age_votes", "males_45age_avg_vote", "males_45age_votes", "females_0age_avg_vote", "females_0age_votes", "females_18age_avg_vote", "females_18age_votes", "females_30age_avg_vote", "females_30age_votes", "females_45age_avg_vote", "females_45age_votes", "top1000_voters_rating", "top1000_voters_votes"], inplace=True)
+movies.drop(columns=["title", "authors", "year", "avg_vote", "votes", "metascore", "usa_gross_income", "reviews_from_users", "reviews_from_critics", "audience_count", "critics_consensus", "date_published", "tomatometer_status", "audience_status", "tomatometer_count", "weighted_average_vote", "median_vote", "tomatometer_top_critics_count", "tomatometer_fresh_critics_count", "tomatometer_rotten_critics_count", "votes_10", "votes_9", "votes_8", "votes_7", "votes_6", "votes_5", "votes_4", "votes_3", "votes_2", "votes_1", "top1000_voters_rating", "top1000_voters_votes","males_0age_avg_vote", "males_0age_votes", "males_18age_avg_vote", "males_18age_votes", "males_30age_avg_vote", "males_30age_votes", "males_45age_avg_vote", "males_45age_votes", "females_0age_avg_vote", "females_0age_votes", "females_18age_avg_vote", "females_18age_votes", "females_30age_avg_vote", "females_30age_votes", "females_45age_avg_vote", "females_45age_votes", "males_allages_avg_vote", "males_allages_votes", "females_allages_avg_vote", "females_allages_votes"], inplace=True)
 
+# drop_cols = "allgenders_0age_votes", "allgenders_0age_avg_vote", "allgenders_18age_avg_vote", "allgenders_18age_votes", "allgenders_30age_avg_vote", "allgenders_30age_votes", "allgenders_45age_avg_vote", "allgenders_45age_votes", "males_0age_avg_vote", "males_0age_votes", "males_18age_avg_vote", "males_18age_votes", "males_30age_avg_vote", "males_30age_votes", "males_45age_avg_vote", "males_45age_votes", "females_0age_avg_vote", "females_0age_votes", "females_18age_avg_vote", "females_18age_votes", "females_30age_avg_vote", "females_30age_votes", "females_45age_avg_vote", "females_45age_votes"
 #fill null values
 movies["country"] = movies["country"].fillna("None")
 movies["genres"] = movies["genres"].fillna("Not defined")
 movies["movie_info"] = movies["movie_info"].fillna(" ")
+movies["audience_rating"] = movies["audience_rating"].fillna("Insuficient votes")
+movies["allgenders_0age_avg_vote"] = movies["allgenders_0age_avg_vote"].fillna("No votes")
+movies["allgenders_0age_votes"] = movies["allgenders_0age_votes"].fillna("No votes")
+movies["allgenders_18age_avg_vote"] = movies["allgenders_18age_avg_vote"].fillna("No votes")
+movies["allgenders_18age_votes"] = movies["allgenders_18age_votes"].fillna("No votes")
 
 movies["directors"] = movies["directors"].fillna("Unknown")
 movies["budget"] = movies["budget"].fillna("Unknown")
@@ -62,16 +68,17 @@ movies.at[8488, 'language'] = 'English'
 movies.at[8921, 'language'] = 'English'
 movies.at[9005, 'language'] = 'English'
 
-#update males_allages_votes
-movies["males_allages_votes"] = movies["males_allages_votes"].apply(np.int64)
-
 #adjusting movies columns order
-movies = movies[["imdb_title_id", "rotten_tomatoes_link", "original_title", "original_release_date", "streaming_release_date", "country", "language", "production_company", "directors", "writer", "actors", "budget", "worlwide_gross_income", "genres", "content_rating", "runtime", "movie_info", "total_votes", "weighted_average_vote", "mean_vote", "median_vote", "males_allages_votes", "males_allages_avg_vote", "females_allages_votes", "females_allages_avg_vote", "us_voters_votes", "us_voters_rating", "non_us_voters_votes", "non_us_voters_rating"]]
+movies = movies[["imdb_title_id", "rotten_tomatoes_link", "original_title", "original_release_date", "streaming_release_date", "country", "language", "production_company", "directors", "writer", "actors", "budget", "worlwide_gross_income", "genres", "content_rating", "runtime", "movie_info", "audience_rating", "tomatometer_rating", "total_votes", "mean_vote", "allgenders_0age_avg_vote", "allgenders_0age_votes", "allgenders_18age_avg_vote", "allgenders_18age_votes", "allgenders_30age_avg_vote", "allgenders_30age_votes", "allgenders_45age_avg_vote", "allgenders_45age_votes", "us_voters_rating", "us_voters_votes", "non_us_voters_rating", "non_us_voters_votes"]]
 
 #comparar colunas
 #movies['is_score_chased'] = np.where(movies['date_published']==movies['original_release_date'], 
 #                                            'yes', 'no')
 
+#check which columns have null values
+# nan_values = movies.isna()
+# nan_columns = nan_values.any()
+# columns_with_nan = movies.columns[nan_columns].tolist()
 
 #reviews
 reviews.dropna(subset=["review_content"], inplace=True, how="all")
