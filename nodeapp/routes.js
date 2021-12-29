0fab87120d337dec916dceba0990d9ecc0ca51d0
Tomás <tomaslouromendes@gmail.com>
2021-12-29 01:18:11 +0000

    movie page almost done (missing review cards)

--- nodeapp/routes.js
+++ nodeapp/routes.js
@@ -34,6 +34,18 @@ router.get("/movie", function(req, res) {
         }
         const response = result.response
 
+        response.docs[0]["original_release_date"] = response.docs[0]["original_release_date"].slice(0,10)
+        response.docs[0]["streaming_release_date"] = response.docs[0]["streaming_release_date"].slice(0,10)
+        response.docs[0]["genres"] = response.docs[0]["genres"].split(",")
+        response.docs[0]["directors"] = response.docs[0]["directors"].split(",")
+        response.docs[0]["writer"] = response.docs[0]["writer"].split(",")
+        response.docs[0]["actors"] = response.docs[0]["actors"].split(",")
+        response.docs[0]["production_company"] = response.docs[0]["production_company"].split(",")
+        response.docs[0]["language"] = response.docs[0]["language"].split(",")
+        response.docs[0]["country"] = response.docs[0]["country"].split(",")
+        // for(let i = 0; i < response.docs[0].genres; i++){
+        //     let aux = "";
+        // }
         movie = response.docs[0]
         console.log(movie)
         res.render("more_info", {movie: movie})
@@ -68,7 +80,7 @@ router.get("/search", (req,res) => {
         const response = result.response
 
         for (let i = 0; i < response.docs.length; i++) {
-            response.docs[i]["original_release_date"] = response.docs[i]["original_release_date"].slice(0,10);
+            response.docs[i]["original_release_date"] = response.docs[i]["original_release_date"].slice(0,10)
         }
 
         res.render("index", {data: {
