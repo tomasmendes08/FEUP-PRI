b063c20ce5a394c80e8a433a67aeb4545603049d
Caio-Nogueira <up201806218@fe.up.pt>
2022-01-10 22:34:24 +0000

    Adds openNLP filters (not working)

--- startup.sh
+++ startup.sh
@@ -5,13 +5,11 @@ precreate-core movies
 precreate-core reviews
 
 # Start Solr in background mode so we can use the API to upload the schema
-solr start "-Denable.runtime.lib=true"
+solr start
 
 sleep 7
 
 cp /data/synonyms.txt /var/solr/data/movies/conf
-
-mkdir /var/solr/data/movies/conf
 
 mkdir /var/solr/data/movies/lib
 
@@ -21,12 +19,17 @@ cp /models/en-sent.bin /var/solr/data/movies/conf/en-sent.bin
 
 cp /models/en-token.bin /var/solr/data/movies/conf/en-token.bin
 
+cp /models/en-chunker.bin /var/solr/data/movies/conf/en-chunker.bin
+
+cp /models/en-pos-maxent.bin /var/solr/data/movies/conf/en-pos-maxent.bin
+
+cp /models/en-lemmatizer-dict.txt /var/solr/data/movies/conf/en-lemmatizer-dict.txt
+
+cp /models/stop.pos.txt /var/solr/data/movies/conf/stop.pos.txt
+
 cp contrib/analysis-extras/lib/icu4j-62.1.jar /var/solr/data/movies/lib
 cp contrib/analysis-extras/lib/opennlp-tools-1.9.2.jar /var/solr/data/movies/lib
 cp contrib/analysis-extras/lib/morfologik-stemming-2.1.5.jar /var/solr/data/movies/lib
-cp contrib/analysis-extras/lib/morfologik-fsa-2.1.5.jar /var/solr/data/movies/lib
-cp contrib/analysis-extras/lib/morfologik-polish-2.1.5.jar /var/solr/data/movies/lib
-cp contrib/analysis-extras/lib/morfologik-ukrainian-search-4.9.1.jar /var/solr/data/movies/lib
 
 cp contrib/analysis-extras/lucene-libs/lucene-analyzers-icu-8.10.1.jar /var/solr/data/movies/lib
 cp contrib/analysis-extras/lucene-libs/lucene-analyzers-morfologik-8.10.1.jar /var/solr/data/movies/lib
